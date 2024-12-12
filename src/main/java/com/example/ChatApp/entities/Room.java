bc0083fce618c2980b82018123b44d3f8d5d9384
Somnath Kundu <kundusomnath610@gmail.com>
2024-12-13 03:06:29 +0530

    First Commit

--- src/main/java/com/example/ChatApp/entities/Room.java
+++ src/main/java/com/example/ChatApp/entities/Room.java
@@ -6,7 +6,7 @@ import java.util.List;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import lombok.AllArgsConstructor;
+import lombok.AllArgsConstructor; 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
